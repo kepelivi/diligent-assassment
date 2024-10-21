b5643f4e8462c8eedad0b510bafdfc83ad07f21f
kepelivi <kepelivi7@gmail.com>
2024-10-21 16:53:17 +0200

    log meaningful error message

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,26 @@
 import { AppError } from './app.error';
-import { list } from './commands';
+import { list, details } from './commands';
 import { Store } from './stores/store.type';
 import { RecipeType } from './recipe';
 
 type Command = (store: Store<RecipeType[]>, args: string[]) => Promise<void>
 
-export async function createApp(store: Store<RecipeType[]>, args: string[], ) {
+export async function createApp(store: Store<RecipeType[]>, args: string[],) {
   const [, , command, ...restArgs] = args;
-  
+
   const commands: Record<string, Command> = {
-    'list': list
+    'list': list,
+    'details': details,
   }
 
-  if(command in commands) {
-    const commandFunction = commands[command] 
-    await commandFunction(store, restArgs);
-  } else {
-    throw new AppError(`Unknown command: ${command}`);
+  try {
+    if (command in commands) {
+      const commandFunction = commands[command]
+      await commandFunction(store, restArgs);
+    } else {
+      throw new AppError(`Unknown command: ${command}`);
+    }
+  } catch (err) {
+    console.error(err)
   }
 }
